078230ebfbd23461b41abb99e66aedac937563d2
Mohammad El Sherbini <mh-sherbini@hotmail.com>
2017-10-22 23:49:30 +0200

    Update 1st program.cs

--- Task 1/1st program.cs
+++ Task 1/1st program.cs
@@ -1,1 +1,63 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Int32.Parse(Console.ReadLine());
+
+         
+            string[] s = { "zero", "one", "two", "three", "four", "five", "six ", "seven", "eight", "nine","ten",
+                "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+            string[] ss = { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
+
+
+           
+                int a = n % 10; 
+                int b = (n / 10) % 10;
+                int c = (n / 100) % 10;
+
+
+         if (n > 100)
+            {
+
+                Console.WriteLine(s[c] + " hundred");
+                if (a != 0)
+
+                    Console.WriteLine("and ");
+
+                n %= 100;
+            }
+               
+             if(n<=19)
+                        Console.WriteLine(s[n]);
+                     
+                  else  if (a == 0)
+                    
+                        Console.WriteLine(ss[b - 2]);
+                    
+                    else
+                    
+                        Console.WriteLine(ss[b - 2] + "-" + s[a]);
+                    
+                
+
+                    
+                }
+            }
+           
+        }
+
+    
+    
+
